331265f215a366e668e386bff0a31badc0d86835
Egor <andrushko.egor@gmail.com>
2018-06-11 07:15:30 +0300

    Add endless scroll

--- js/main.js
+++ js/main.js
@@ -10,48 +10,6 @@ document.getElementById('search').onclick = function(event){
     return;
   }
 }
-const container = document.getElementById('content');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-container.addEventListener('mousedown', (event) => {
-  isDown = true;
-  startX = event.pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
-});
-
-container.addEventListener('mouseleave', () => isDown = false);
-
-container.addEventListener('mouseup', () => {
-  isDown = false;
-  let card = document.getElementsByClassName('card')[0];
-  var width = card.offsetWidth + parseInt(getComputedStyle(card).marginRight) * 2;
-  if (container.scrollLeft < width/2) {
-    container.scrollLeft = 0;
-  }
-  else {
-    if (container.scrollLeft > width/2 && container.scrollLeft < width){
-      container.scrollLeft = width;
-    }
-    else{
-      if(container.scrollLeft%width < width/2){
-        container.scrollLeft -= container.scrollLeft%width;
-      }
-      else{
-        container.scrollLeft += width-container.scrollLeft%width;
-      }
-    }
-  }
-});
-
-container.addEventListener('mousemove', (event) => {
-  if(!isDown) return;
-  event.preventDefault();
-  const x = event.pageX - container.offsetLeft;
-  let delta = x - startX;
-  container.scrollLeft = scrollLeft - delta * 2;
-});
 
 function start() {
     gapi.client.init({
@@ -64,34 +22,85 @@ function loadClient() {
   gapi.load('client', start);
 }
 
-function search(query) {
+function search(query,pageToken) {
       return gapi.client.request({
       'method': 'GET',
       'path': '/youtube/v3/search',
       'params': {'maxResults': '20',
                  'part': 'snippet',
                  'q': query,
-                 'type': 'video'
+                 'type': 'video',
+                 'pageToken': pageToken
                 }
     }).then(function(response) {
-          console.log(response.result);
           let block, title, img, video, url, a, card;
           video = response.result.items;
           for (var i = 0; i < video.length; i++) {
             card = `
             <div class="card">
-              <a href="https://www.youtube.com/watch?v="${video[i].id.videoId}">
+              <a href="https://www.youtube.com/watch?v=${video[i].id.videoId}" target="_blank">
                 <img class="thumbnail" src="${video[i].snippet.thumbnails.medium.url}" />
               </a>
               <div class="card-bottom">
                 <h2>${video[i].snippet.title}</h2>
-                <p>${video[i].snippet.description}</p>
+                <p class="description">${video[i].snippet.description}</p>
+                <p class="author">${video[i].snippet.channelTitle}</p>
               </div>
             </div>`;      
-            container.innerHTML += card;
+            document.getElementById('content').innerHTML += card;
           }
+          StartScroll(query, response.result.nextPageToken);
         }, function(reason) {
           console.log('Error: ' + reason.result.error.message);
         });
 }
 loadClient();
+function StartScroll(query, nextPageToken){
+  const container = document.getElementById('content');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  container.addEventListener('mousedown', (event) => {
+    isDown = true;
+    startX = event.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+  container.addEventListener('mouseleave', () => isDown = false);
+
+  container.addEventListener('mouseup', () => {
+    isDown = false;
+    let card = document.getElementsByClassName('card')[0];
+    var width = card.offsetWidth + parseInt(getComputedStyle(card).marginRight) * 2;
+    if (container.scrollLeft < width/2) {
+      container.scrollLeft = 0;
+    }
+    else {
+      if (container.scrollLeft > width/2 && container.scrollLeft < width){
+        container.scrollLeft = width;
+      }
+      else{
+        if(container.scrollLeft%width < width/2){
+          container.scrollLeft -= container.scrollLeft%width;
+        }
+        else{
+          container.scrollLeft += width-container.scrollLeft%width;
+        }
+      }
+    }
+  });
+
+  container.addEventListener('mousemove', (event) => {
+    if(!isDown) return;
+    event.preventDefault();
+    const x = event.pageX - container.offsetLeft;
+    let delta = x - startX;
+    container.scrollLeft = scrollLeft - delta;
+  });
+
+  container.onscroll = function(){
+    if(container.scrollLeft-container.scrollWidth+container.clientWidth == 0){
+      search(query,nextPageToken);
+    }
+  }
+}
